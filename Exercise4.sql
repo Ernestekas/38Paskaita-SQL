d99b841f20a3b3a3af60ca673146d0393e7e09bb
ernestekas <ernestekas@gmail.com>
2021-11-05 21:12:25 +0200

    Misreadings corrected in some exercises.

--- Exercise4.sql
+++ Exercise4.sql
@@ -2,4 +2,5 @@ USE lessons;
 
 SELECT orderid, CONVERT(DECIMAL(10,2),unitprice*qty) AS totalvalue
 FROM Sales.OrderDetails
-WHERE unitprice*qty > 10000;
+WHERE unitprice*qty > 10000
+ORDER BY totalvalue;
--- Exercise9.sql
+++ Exercise9.sql
@@ -2,4 +2,4 @@ USE lessons;
 
 SELECT custid, region
 FROM Sales.Customers
-ORDER BY CASE WHEN region is null then 1 else 0 end, region
+ORDER BY CASE WHEN region is null then 1 else 0 end, region ASC
